65ae029c131fed9776d3d1b410fe4a5f93f6be06
Sergio Vavassori <svavassori@conwet.com>
2014-11-28 21:03:45 +0100

    Sort loading NPM Tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,9 +20,10 @@ module.exports = function(grunt) {
     compress: {
       widget: {
         options: {
+          archive: 'build/<%= pkg.vendor %>_<%= pkg.name %>_<%= pkg.version %>-dev.wgt',
           mode: 'zip',
           level: 9,
-          archive: 'build/<%= pkg.vendor %>_<%= pkg.name %>_<%= pkg.version %>-dev.wgt'
+          pretty: true
         },
         files: [
           {expand: true, src: ['lib/**/*', 'config.xml', 'index.html', 'js/**/*', 'css/**/*'], cwd: 'src'},
@@ -61,15 +62,15 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-text-replace');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-text-replace');
 
   grunt.registerTask('zip', 'compress:widget');
   grunt.registerTask('version', ['replace:version']);
