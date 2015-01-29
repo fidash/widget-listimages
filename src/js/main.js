a1aa4a86847f245d78c8af4644a2723d5efac56c
bgrana <bgrana@conwet.com>
2015-01-29 08:11:22 +0100

    Add refresh button

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,10 @@
-/* global OpenStackProto */
+/* global OpenStackProto DataViewer */
 
 $(document).ready(function() {
     "use strict";
 
 	var openStackProto = new OpenStackProto();
     var myTable = new DataViewer();
-    /*var refresh = $('<button>');
-    var filter = $("#example_filter");
-
-
-    refresh.text('Refresh');
-    refresh.css({
-    	float: 'left',
-    	'margin-left': '10px'
-    });
-    refresh.click(function () {
-    	openStackProto.listImage(myTable);
-    });
-    refresh.insertBefore(filter);*/
 
     myTable.init();
     openStackProto.init();
--- src/js/openstack-proto.js
+++ src/js/openstack-proto.js
@@ -3,7 +3,6 @@ var OpenStackProto = (function (JSTACK) {
 
     var isAuthenticated = false;
     var dataViewer = null;
-    //var url = 'http://cloud.testbed.fi-ware.org/keystone/v2.0/';
     var url = 'https://cloud.lab.fiware.org/keystone/v2.0/';
 
     function authenticate () {
@@ -13,10 +12,6 @@ var OpenStackProto = (function (JSTACK) {
         var options;
         var USERNAME, PASSWORD;
         var headersTenants = {};
-
-        //tokenId = MashupPlatform.context.get('fiware_user_token');
-        //tokenId = "Sd27fIXLknxG4krOveBu9rHOQYl3BCu8Wghuy3va0PP3V8pEEp-6LBWBWds5NtVWtIBPlZVFjW9GTiJhhRGeZA";
-        //tenantId = "00000000000000000000000000002312";
 
         headersAuth = {
             "Accept": "application/json",
@@ -33,12 +28,13 @@ var OpenStackProto = (function (JSTACK) {
 
         postBody.auth.token = {
             "id": "%fiware_token%"
-            //"id": "Sd27fIXLknxG4krOveBu9rHOQYl3BCu8Wghuy3va0PP3V8pEEp-6LBWBWds5NtVWtIBPlZVFjW9GTiJhhRGeZA",
-            //"tenantId":"00000000000000000000000000002312"
         };
 
 
+        // Initialize Keystone
         JSTACK.Keystone.init(url);
+
+        // Get tenants with the user's FIWARE token
         MashupPlatform.http.makeRequest(url + 'tenants', {
             method: 'GET',
             requestHeaders: headersTenants,
@@ -46,16 +42,20 @@ var OpenStackProto = (function (JSTACK) {
 
                 response = JSON.parse(response.responseText);
                 postBody.auth.tenantId = response.tenants[0].id;
+
+                // Post request to receive service token from Openstack
                 MashupPlatform.http.makeRequest(url + 'tokens', {
                     requestHeaders: headersAuth,
                     contentType: "application/json",
                     postBody: JSON.stringify(postBody),
                     onSuccess: function (response) {
                         response = JSON.parse(response.responseText);
+
+                        // Mimic JSTACK.Keystone.authenticate behavior on success
                         JSTACK.Keystone.params.token = response.access.token.id;
                         JSTACK.Keystone.params.access = response.access;
-                        JSTACK.Keystone.params.currentstate = 2;
-                        //handleTempToken(response);
+                        JSTACK.Keystone.params.currentstate = 2;                      
+
                         handleServiceToken();
                     },
                     onFailure: function (response) {
@@ -72,60 +72,6 @@ var OpenStackProto = (function (JSTACK) {
         
         //JSTACK.Keystone.authenticate(USERNAME, PASSWORD, tokenId, tenantId, handleTempToken, onError);
     }
-
-
-// RESULT
-// {
-//     "access": {
-//         "metadata": {
-//             "is_admin": 0, 
-//             "roles": []
-//         }, 
-//         "serviceCatalog": [], 
-//         "token": {
-//             "audit_ids": [
-//                 "C8W4Eg4tQA2cYihJUXT3Dw"
-//             ], 
-//             "expires": "2014-11-25T12:32:17Z", 
-//             "id": "PKIZ_eJxtVEuzojwQ3fMrZm_dEtCLl2V4xSiJIhIeOwHlDd5BDPjrJ3K_r2YzVKVIdzrdp0-fyscH_zQTIvJLx-7b-BAwQsYuC3f7LkL5MyHASfSO-6yXroMR68AxR-MMbC2rs7zKtMjBJshMlmWWqGXVt5BXBVSZqGkMZtxZawhpYdAXoU_KKCAvVHbZddqJsfyo4wIpaNsXF-5PfVoFsiUK7wBUIobPzgOfzQcuqcf_Cn4hBZ-TARvemhiZhAqWoYauE1hPUYCLQwEKXIJRILo4YlecCHVGXHYvvkYidYWtg-LiO3NgAsc8lGkXy2MVBWg-CwNaXYLTTbj4pxmmP-2Mg5WIJz8RU8vScEOii5_vKc09LJ5WqJbU98VEplPa1GXkkTyFVi_EcsKraHHgcogtfb0rcHvzs98NoS_Vc5vNbuJV-4v_NSNIGrXnHN2EGNbDZYX_Jmnq6oeXqCFGJUZlssJnMBLDkXFpfkalVoUl_iSyVRygxwRcVmwmqN09Y0h_2pHEHun_rn6bi0g1h1-FwSkX_hvTPZHUPILSfR6VAd4JJk5anUzcth7pLZBUHCItP3N9hOZbHywwtEDATs90JzSo40CT7aj3Mg8YMAgkj0cxi_qfnAtnNEqAtYxQDSRYo3TgZIrYWTPBAPPlvQEojVtSp4YZYi2cEwDGjukqXdl8_rwdFkNvCGX1wZWoYdBD3deh4AKrBszRwRmQtzT_VyaHaAHgcqjaduyX8faWmCm8nIfXXemGnQ5VpV945Ch4iPRfg3v8PMpglanxfdfu26dUgoPkAmUYA_JtXsJNhKk-3i-uF4vt3v5tXMut0qrmMRSy-hgs7Ih0i3Dlmknj3UVGkivEwL7tbSIewwGn6xaej3izpvXCd92DdnBJkGKnHbOVUDnJzTisT2NXbm9qni8qex83YZ-mVsfQwVlev8CLSoWRgNHaTKTw_WvuKctn_iT35ToVlqMp0-Wjuy42uQMVZ-it31wNC3u8KWnpnC1gdNtUsW20ybbpULqn6bl4gS-r0Rfrgd6EoT0ZgbrVfOVaWbUXY_nU2P2ztoel0m1bcmKMxKZfhNN3xY5j0ql2nVVQEubnxCTG36flD8Znc1c=", 
-//             "issued_at": "2014-11-25T11:32:17.058485"
-//         }, 
-//         "user": {
-//             "id": "6f494f77101446219f0db397ab8e0290", 
-//             "name": "braulio", 
-//             "roles": [], 
-//             "roles_links": [], 
-//             "username": "braulio"
-//         }
-//     }
-// }
-    // function handleTempToken (result) {
-    //     // curl -s http://arcturus.ls.fi.upm.es:5000/v2.0/tenants -X 'GET' -H "X-Auth-Token: $token" "Content-Type: application/json" | python -m json.tool
-    //     var isAdmin = false;
-    //     var tokenId = result.access.token.id;
-    //     JSTACK.Keystone.gettenants(handleTenants.bind(null, tokenId), isAdmin, onError);
-    // }
-
-// RESULT
-// {
-//     "tenants": [
-//         {
-//             "description": null, 
-//             "enabled": true, 
-//             "id": "efdee1fee7b849af9c51967770cd21fb", 
-//             "name": "demo"
-//         }
-//     ], 
-//     "tenants_links": []
-// }
-    // function handleTenants (tokenId, result) {
-    //     // curl -s http://arcturus.ls.fi.upm.es:5000/v2.0/tenants -X 'GET' -H "X-Auth-Token: $token" "Content-Type: application/json" | python -m json.tool
-    //     var tenantId = result.tenants[0].id;
-    //     var username, password;
-
-    //     JSTACK.Keystone.authenticate(username, password, tokenId, tenantId, handleServiceToken, onError);
-    // }
-
 
     function handleServiceToken () {
 
@@ -157,13 +103,25 @@ var OpenStackProto = (function (JSTACK) {
         var structure = [ {'id': 'name'}, {'id': 'status'}, {'id': 'updated'} ];
         var data = [];
         var dataset = {'structure': structure, 'data': data};
+        var refresh;
 
+        // Gather data from GET response
         for (var imageKey in result.images) {
             var image = result.images[imageKey];
-            data.push({'name': image.name, 'status': image.status, 'updated': image.updated});
+            data.push({'name': image.name, 'status': image.status, 'updated': image.updated_at});
         }
+
+        // Create table with the given dataset
         table.setModel(dataset);
 
+        // Set refresh button
+        refresh = new StyledElements.StyledButton({
+            text: 'Refresh'
+        });
+        refresh.addEventListener('click', doWork);
+        var pagination = document.querySelector("div.container.pagination");
+        refresh.insertInto(pagination);
+        table.layout.repaint();
     }
 
     function onError (error) {
